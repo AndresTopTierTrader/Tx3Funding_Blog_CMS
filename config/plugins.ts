196c0784f552039366428d3a324c6b6c98080f94
Andrés Poveda <153770860+AndresTopTierTrader@users.noreply.github.com>
2025-08-25 15:26:30 -0300

    fix: add JWT secret configuration for users-permissions plugin

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,1 +1,15 @@
-export default () => ({});
+export default ({ env }) => ({
+  'users-permissions': {
+    config: {
+      jwtSecret: env('JWT_SECRET'),
+    },
+  },
+  upload: {
+    config: {
+      provider: 'local',
+      providerOptions: {
+        sizeLimit: 100000,
+      },
+    },
+  },
+});
