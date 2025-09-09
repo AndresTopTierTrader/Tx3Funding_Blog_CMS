5be3eb7b0f19dac4530791b031ce172f30df24a1
Andrés Poveda <153770860+AndresTopTierTrader@users.noreply.github.com>
2025-09-09 08:28:13 -0500

    feat: add cloudinary config

--- config/plugins.ts
+++ config/plugins.ts
@@ -6,8 +6,17 @@ export default ({ env }) => ({
   },
   upload: {
     config: {
-      provider: 'local',
-      sizeLimit: 100000,
+      provider: 'cloudinary',
+      providerOptions: {
+        cloud_name: env('CLOUDINARY_NAME'),
+        api_key: env('CLOUDINARY_KEY'),
+        api_secret: env('CLOUDINARY_SECRET'),
+      },
+      actionOptions: {
+        upload: {},
+        uploadStream: {},
+        delete: {},
+      },
     },
   },
 });
